5b749a29d74a547db7f47988b6a91cf2c7c62cbb
Ryker Sun <rykersun2002@gmail.com>
2025-11-14 14:10:49 +0800

    Try fixing build failed problem

--- src/components/common/shortcodes/Youtube.tsx
+++ src/components/common/shortcodes/Youtube.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import LiteYouTubeEmbed from "react-lite-youtube-embed";
+// import LiteYouTubeEmbed from "react-lite-youtube-embed";
+import { LiteYouTubeEmbed } from "react-lite-youtube-embed";
 import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 
 const Youtube = ({
